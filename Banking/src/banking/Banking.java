1db58ce4c820fb4a5c6495dc1b8c2291a73fc95e
Rashmi Kattimani <rrkattimani47@github.com>
2018-05-29 15:50:06 +0530

    bank commit

--- Banking/src/banking/Banking.java
+++ Banking/src/banking/Banking.java
@@ -4,18 +4,39 @@
  * and open the template in the editor.
  */
 package banking;
-
+import java.util.Scanner;
 /**
  *
  * @author rashmikattimani47
  */
 public class Banking {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+        int choice;
+        Account b=new Account();
+        while(true){
+            System.out.println("Enter choice\n1.Create\n2.Deposit\n3.Withdraw\n4.Balance Enquiry\n5.Exit\n");
+            Scanner s=new Scanner(System.in);
+            choice=s.nextInt();
+            switch(choice){
+                case 1:b.openAccount();
+                        break;
+                case 2:b.deposit();
+                        break;
+                case 3:b.withdrawal();
+                        break;
+                case 4:b.balanceEnquiry();
+                        break;
+                default:System.exit(0);
+      
+            }           
+                
+            }
+        }
     }
     
-}
+    
+   
+       
+    
+    
+
